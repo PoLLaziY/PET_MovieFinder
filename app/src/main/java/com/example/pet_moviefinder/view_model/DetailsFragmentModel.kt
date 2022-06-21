fa67c1f12fa52168c1d669781e45ef1b2a0441c9
PoLLaziY <xlanzerx@gmail.com>
2022-06-22 01:08:54 +0300

    Add LiveData<List<Film>> to all viewmodel and model classes

--- app/src/main/java/com/example/pet_moviefinder/view_model/DetailsFragmentModel.kt
+++ app/src/main/java/com/example/pet_moviefinder/view_model/DetailsFragmentModel.kt
@@ -1,12 +1,12 @@
 package com.example.pet_moviefinder.view_model
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.pet_moviefinder.model.IFavoriteRepositoryController
 import com.example.pet_moviefinder.data.entity.Film
 import com.example.pet_moviefinder.model.INavigationController
-import com.example.pet_moviefinder.model.NavigationController
 
 class DetailsFragmentModel(
     val favoriteController: IFavoriteRepositoryController,
@@ -15,34 +15,42 @@ class DetailsFragmentModel(
 ) : ViewModel() {
 
     val film: MutableLiveData<Film?> = MutableLiveData<Film?>()
-
-    private var _isFavoriteInRepository: Boolean = false
-
-    val isFavorite: MutableLiveData<Boolean> = MutableLiveData(false)
-
-    init {
-        film.observeForever {
+    private val filmObserver = Observer<Film?> { it ->
             _isFavoriteInRepository = favoriteController.isFavorite(it)
             isFavorite.postValue(_isFavoriteInRepository)
             if (it != null) handle.set(SavedStateHandleKeys.DETAILS_FILM_KEY, it)
         }
 
-        film.postValue(handle.get(SavedStateHandleKeys.DETAILS_FILM_KEY))
+    private var _isFavoriteInRepository: Boolean = false
 
-        isFavorite.observeForever {
-            if (it && !_isFavoriteInRepository) addToFavorite(film.value)
-            if (!it && _isFavoriteInRepository) removeFromFavorite(film.value)
-        }
+    val isFavorite: MutableLiveData<Boolean> = MutableLiveData(false)
+    private val isFavoriteObserver = Observer<Boolean> { it ->
+        if (it == true && !_isFavoriteInRepository) addToFavorite(film.value)
+        if (it != true && _isFavoriteInRepository) removeFromFavorite(film.value)
     }
 
-    fun addToFavorite(film: Film?) {
+    init {
+        film.observeForever (filmObserver)
+
+        film.postValue(handle.get(SavedStateHandleKeys.DETAILS_FILM_KEY))
+
+        isFavorite.observeForever (isFavoriteObserver)
+    }
+
+    override fun onCleared() {
+        film.removeObserver(filmObserver)
+        isFavorite.removeObserver(isFavoriteObserver)
+        super.onCleared()
+    }
+
+    private fun addToFavorite(film: Film?) {
         if (film != null) {
             favoriteController.addFavorite(film)
             _isFavoriteInRepository = true
         }
     }
 
-    fun removeFromFavorite(film: Film?) {
+    private fun removeFromFavorite(film: Film?) {
         if (film != null) {
             favoriteController.removeFavorite(film)
             _isFavoriteInRepository = false
